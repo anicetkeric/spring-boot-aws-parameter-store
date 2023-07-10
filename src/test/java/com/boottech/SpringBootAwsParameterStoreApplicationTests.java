cf14863bd1a927fab8d18a63c778d4f056f4af88
aek <anicetkeric@outlook.com>
2023-07-10 21:26:02 +0200

    initial commit

---
+++ src/test/java/com/boottech/SpringBootAwsParameterStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boottech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsParameterStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
